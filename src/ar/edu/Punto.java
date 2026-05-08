62215940601d45f5364fc63b3430f55d74dabb41
Rocio Belen Mamani <rocidxs@gmail.com>
2026-05-08 16:12:59 -0300

    primero commit

---
+++ src/ar/edu/Punto.java
@@ -0,0 +1,25 @@
+package ar.edu;
+
+public class Punto {
+	private float x;
+	private float y;
+	
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public Punto(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+}
